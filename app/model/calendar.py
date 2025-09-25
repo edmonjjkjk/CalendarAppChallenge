d309449acc13ed1aa7c84179311f0b8f061f68d6
Eritz <Martinezmuriel2523@gmail.com>
2025-09-25 17:31:28 -0500

    comit class Remider y class Event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -1,6 +1,9 @@
+
 from dataclasses import dataclass, field
 from datetime import datetime, date, time
 from typing import ClassVar
+
+
 
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
     reminder_not_found_error, slot_not_available_error
@@ -16,3 +19,35 @@ from app.services.util import generate_unique_id, date_lower_than_today_error, e
 
 
 # TODO: Implement Calendar class here
+@dataclass
+class Reminder:
+    EMAIL: str = field(init= False, default = "email")
+    SYSTEM: str = field(init=False, default= "system")
+
+    date_time:  datetime
+    type: str = ClassVar[EMAIL]
+
+    def __str__(self)-> str:
+        return f"Reminder on {self.date_time} of type {self.type}"
+@dataclass()
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(init=False, default= list)
+    id: str = field(default_factory = generate_unique_id)
+
+
+
+
+
+
+
+
+
+
+
+
+

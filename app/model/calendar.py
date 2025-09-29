74995b2cdd25bec0bc79ca081b3330dc35694c04
tomas_k0bjeoq <Martinezmuriel2523@gmail.com>
2025-09-29 15:13:46 -0500

    comit the class day

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -1,4 +1,4 @@
-
+from calendar import Day
 from dataclasses import dataclass, field
 from datetime import datetime, date, time
 from typing import ClassVar
@@ -41,6 +41,58 @@ class Event:
 
     def add__reminder(self, date_time: datetime, type: str):
         self.reminders.append(Reminder(date_time, type))
+    def deleate_reminder(self, Reminder_index: int):
+        Reminder_index: int = Reminder_index
+        for reminder in self.reminders:
+            pass
+
+class Day:
+    date_: date
+    slots: dict[time,str | None ]
+    def __init__(self, date_: date):
+        self.date_ = date_
+        self.slots = []
+
+
+    def _init_slots(self):
+        pass
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        pass
+    def delete_event(self, event_id: str):
+        pass
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        pass
+
+
+
+
+
+
+    class Calendar:
+        days: dict[date, Day]
+        def __init__(self):
+            pass
+        def add_event(self, title: str, description: str, date_: date, start_at: time, end_at: time)-> str:
+            pass
+        def update_event(self,event_id: str, title: str, description: str, date_: date, start_at: time, end_at: time ):
+            pass
+        def delete_event(self,event_id: str):
+            pass
+        def fid_events(self,start_at: time, end_at: date)-> dict[date, list["Event"]]:
+            pass
+        def add_reminder(self, event_id: str,date_time: datetime, type: str):
+            pass
+        def delete_reminder(self, event_id: str, Reminder_index: int):
+            pass
+        def list_reminders(self, event_id: str)-> list[Reminder]:
+            pass
+        def find_available_slots(self,date_: date) -> list[time]:
+            pass
+
+
+
+
+
 
 
 
